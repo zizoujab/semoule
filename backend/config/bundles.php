00a3f159319f63a8e78e0d6c5a21f1133a111798
Zied Jaballah <zied.jabalah@gmail.com>
2024-10-06 02:12:35 +0200

    backend

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
